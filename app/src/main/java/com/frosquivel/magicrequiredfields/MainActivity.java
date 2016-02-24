287c49641d837da41da96e4be41890139e5fed37
Fabian Rosales <fabian7593@gmail.com>
2016-02-23 23:58:54 -0600

    Frosales, the first uinterface with a simple example

--- app/src/main/java/com/frosquivel/magicrequiredfields/MainActivity.java
+++ app/src/main/java/com/frosquivel/magicrequiredfields/MainActivity.java
@@ -2,6 +2,9 @@ package com.frosquivel.magicrequiredfields;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerTest);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        EditText editText = (EditText) findViewById(R.id.editTest);
     }
 }
